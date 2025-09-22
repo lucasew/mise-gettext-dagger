ace5a63133e06dd18746d254515396fa04fa78ce
lucasew <lucas59356@gmail.com>
2025-09-22 14:17:26 -0300

    genesis
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ .dagger/src/mise_gettext_dagger/main.py
@@ -0,0 +1,105 @@
+import dagger
+from dagger import dag, function, object_type
+import requests
+from dataclasses import dataclass
+import bs4
+import re
+from bs4 import BeautifulSoup
+from collections import defaultdict
+from typing import Iterable
+
+# @object_type
+@dataclass
+class GettextVersion:
+    tarball_url: str
+    sig_url: str
+    version: str
+
+    @staticmethod
+    def base_url() -> str:
+        return "https://ftp.gnu.org/gnu/gettext/"
+
+    @staticmethod
+    def tarball_base_url() -> str:
+        # return "https://ftp.gnu.org/gnu/gettext/gettext-%version%.tar.gz"
+        return "https://github.com/autotools-mirror/gettext/archive/refs/tags/v%version%.tar.gz"
+    
+    @staticmethod
+    def from_version(version: str) -> 'GettextVersion':
+        return GettextVersion(
+            version=version,
+            tarball_url=GettextVersion.tarball_base_url().replace('%version%', version),
+            sig_url=GettextVersion.base_url() + f"gettext-{version}.tar.gz.sig"
+        )
+
+    @staticmethod
+    def get_versions() -> Iterable['GettextVersion']:
+        regex = r"gettext-(?P<version>.*?).tar.gz(?P<sig>\.sig)?"
+        res = requests.get(GettextVersion.base_url())
+        tree = BeautifulSoup(res.text)
+        versions = defaultdict(lambda: {})
+        for item in tree.find("table").children:
+            # print(type(item))
+            item = item.find('a')
+            if type(item) is int:
+                continue
+            if item is None:
+                continue
+            name = item.text
+            href = item.get('href')
+            match = re.match(regex, name)
+            if match is None:
+                continue
+            match = match.groupdict()
+            has_sig = match['sig'] is not None
+            versions[match['version']]['sig_url' if has_sig else 'tarball_url'] = href
+        print(versions)
+        for (k, v) in versions.items():
+            yield GettextVersion(
+                tarball_url=GettextVersion.base_url() + v['tarball_url'],
+                sig_url=GettextVersion.base_url() + v['sig_url'],
+                version=k
+            )
+
+@object_type
+class MiseGettextDagger:
+    @function
+    async def teste_build(self, version: str = "0.26") -> dagger.Directory:
+        source = await self.fetch_source(GettextVersion.from_version(version))
+        return source
+
+    @function
+    async def fetch_tarball(self, tarball: dagger.File, signature: dagger.File) -> dagger.Directory:
+        return (
+            await dag.container()
+            .from_("alpine:latest")
+            .with_exec(["apk", "add", "gnupg"])
+            .with_file("/source.tar", tarball)
+            .with_file("/signature.sig", signature)
+            .with_exec(["mkdir", "-p", "/src"])
+            .with_exec(["tar", "-xvf", "/source.tar", "-C", "/src", "--strip-components", "1"])
+            .directory("/src")
+        )
+
+    def fetch_source(self, version: GettextVersion) -> dagger.Directory:
+        source = self.fetch_tarball(
+            tarball=dag.http(version.tarball_url),
+            signature=dag.http(version.sig_url)
+        )
+        return source
+            
+    
+    @function
+    def base_container(self) -> dagger.Container:
+        return dag.container().from_("debian:stable")
+
+    @function
+    def build_linux(self, source: dagger.Directory) -> dagger.Directory:
+        return source # TODO: fazer
+
+    @function
+    def git_clone(self):
+        return dag.git("https://git.savannah.gnu.org/git/gettext.git", keep_git_dir=True)
+
+if __name__ == '__main__':
+    print(list(GettextVersion.get_versions()))
